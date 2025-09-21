e3ff686d82fb58af4b366d390412650830372893
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 16:27:47 +0530

    Q22 solved and added with manual method

--- week2/Q22.py
+++ week2/Q22.py
@@ -1,2 +1,25 @@
 # 22. Write a function to check if a string is an anagram.
 
+
+# s1 = input()
+# s2 = input()
+# def anagram(s1, s2):
+#     return sorted(s1)==sorted(s2)
+
+# print(anagram(s1,s2))
+
+s1 = input()
+s2 = input()
+
+def anagram(s1,s2):
+    if len(s1)!= len(s2):
+        return False
+    count1 ={}
+    count2 ={}
+    for ch in s1:
+        count1[ch] = count1.get(ch,0) +1
+    for ch in s2:
+        count2[ch] = count2.get(ch, 0) +1
+    return count1 == count2
+
+print(anagram(s1, s2))
