e969bcdc8e85963cbfef0cbf0cb52b418625f5dc
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 16:35:05 +0530

    Q23 solved and added

--- week2/Q23.py
+++ week2/Q23.py
@@ -1,2 +1,8 @@
 # 23. Create a nested dictionary to represent student records.
 
+student = {
+    "001" :{"name": "Ali", "age": 20, "grade": "A"},
+    "002": {"name": "Sara", "age": 21, "grade": "B"},
+}
+
+print(student["001"]["name"])
