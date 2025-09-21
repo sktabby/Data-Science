83a0dea11de194e9d44c550911f3489f4d0554b5
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 16:49:20 +0530

    Q25 solved and added with manual method

--- week2/Q25.py
+++ week2/Q25.py
@@ -1,2 +1,21 @@
 # 25. Write a program to find the second highest number in a list.
 
+# numbers = [5, 2, 9, 1, 7]
+# numbers.sort(reverse=True)
+# print(numbers[1])
+
+
+
+numbers = [5, 2, 9, 1, 7]
+
+first = second = float('-inf')
+
+for num in numbers:
+    if num > first:
+        second = first
+        first = num
+    elif num > second and num != first:
+        second = num
+
+print("Second higher number is :", second)
+
