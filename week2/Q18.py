b21886c2547296b76a0fb63f64684a7d365851e0
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 14:39:41 +0530

    Q18 solved and added

--- week2/Q18.py
+++ week2/Q18.py
@@ -1,2 +1,6 @@
 # 18. Create a dictionary and iterate over its keys and values.
 
+students ={"Name": "Tabish", "Age": 22,"Course": "BE"}
+
+for key , value in students.items():
+    print(key ,":", value)
