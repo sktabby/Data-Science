af7c2803992011a910823af214ace95a6f5b9396
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 14:58:04 +0530

    Q19 solved and added with new method

--- week2/Q19.py
+++ week2/Q19.py
@@ -1,9 +1,17 @@
 # 19. Write a function to remove all punctuation from a string.
 
-import string
+# import string
 
-def no_punc(text):
-    return "".join(ch for ch in text if ch not in string.punctuation)
+# def no_punc(text):
+#     return "".join(ch for ch in text if ch not in string.punctuation)
 
-print(no_punc("Hellooo!!!!!! Madammmmmmm!!!"))
+# print(no_punc("Hellooo!!!!!! Madammmmmmm!!!"))
 
+Text = input("Enter the text to remove punctuations in it: ")
+
+def no_punc(Text):
+    punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    result = "".join(ch for ch in Text if ch not in punctuation)
+    return result
+
+print(no_punc(Text))
