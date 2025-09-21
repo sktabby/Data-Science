b5ba8d456463dc54fe70b1345e08f4b3f9c2a454
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 14:16:59 +0530

    Updated file of Q16

--- week2/Q16.py
+++ week2/Q16.py
@@ -1,2 +1,7 @@
 # 16. Create a function that checks whether a string is a palindrome.
+s = input("Enter the Text to check :")
+def palindrom(s):
+    s = s.upper()
+    return s==s[::-1]
 
+print(palindrom(s))
