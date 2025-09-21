954907b4a1bae42cf605b72f2b107a339212db33
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 16:08:09 +0530

    Q21 solved and added with manual method

--- week2/Q21.py
+++ week2/Q21.py
@@ -1,2 +1,12 @@
 # 21. Create a list comprehension to get squares of all even numbers in a range.
 
+# even_square = [x**2 for x in range(1,11) if x %2 ==0]
+# print(even_square)
+
+# for input range
+
+start = int(input("Enter the starting range: "))
+end = int(input("Enter the ending range: "))
+
+e_square = [x**2 for x in range(start, end+1) if x %2 ==0]
+print(e_square)
