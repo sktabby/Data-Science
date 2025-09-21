00d809e7c7aa2e88c007b6976ebc871598cdc673
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 14:23:08 +0530

    Q17 solved and added

--- week2/Q17.py
+++ week2/Q17.py
@@ -1,2 +1,13 @@
 # 17. Write a function to count vowels in a string.
 
+s= input("Enter the Text to count the vowels: ")
+
+def c_v(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for ch in s:
+        if  ch in vowels:
+            count +=1
+    return count
+
+print("Count of vowels is: ", c_v(s))
