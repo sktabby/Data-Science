5a070888b8d4930e5d982d99c1afb80b1a59388a
Shaikh Tabish <shaikhtabish936@gmail.com>
2025-09-21 15:33:59 +0530

    Q20 solved and added with manual method

--- week2/Q20.py
+++ week2/Q20.py
@@ -1,8 +1,30 @@
 # 20. Write a function to capitalize the first letter of each word in a string.
 
-sentance = input("Enter the strinf to capitalize 1st letter: ")
+# sentance = input("Enter the strinf to capitalize 1st letter: ")
+
+# def capital(sentance):
+#     return sentance.title()
+
+# print(capital(sentance))
+
+# using the manual method for capitalization
+
+sentance = input("Enter the text to capitalize first letter: ")
 
 def capital(sentance):
-    return sentance.title()
+    capital_next = True
+    result=""
+
+    for ch in sentance:
+        if ch ==" ":
+            result +=ch
+            capital_next =True
+        else:
+            if capital_next:
+                if "a"<=ch<="z":
+                    ch = chr(ord(ch)-32)
+                capital_next=False
+            result +=ch
+    return result
 
 print(capital(sentance))
